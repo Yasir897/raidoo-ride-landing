f757370cc5790e3ad62890498e8471aea7ec4935
YASH <135501472+Yasir897@users.noreply.github.com>
2025-10-12 21:39:57 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/raidoo-ride-landing/", 
   server: {
-    host: "::",
+    host: ":",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
